e7e371ea921126b8bf1b47e9c6f001e6f8f089e6
umgonji <umgonji@naver.com>
2020-05-30 14:00:39 +0900

    XML에 리스트뷰 배치 + 목록으로 부려줄 데이터 클래스 생성

---
+++ app/src/main/java/dasdsa/sdn/listview_20200530/datas/Student.kt
@@ -0,0 +1,8 @@
+package dasdsa.sdn.listview_20200530.datas
+
+class Student {
+
+    val name = "미입력" //이름이 기본적으로 String이 되도록
+    val brithYear = 0 //생년은 기본으로 Int 가 되게
+    val isMale = true   //성별을  boolaed
+}
