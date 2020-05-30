960dff0e9d717ff12d574b71c29437c2f7de4780
umgonji <umgonji@naver.com>
2020-05-30 15:46:26 +0900

    어댑터 객체 생성 => 리스트뷰 연결  - lateinit : 나중에 객체화.  - 만들어둔 Adapter 생성자 실제 활용

--- app/src/main/java/dasdsa/sdn/listview_20200530/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/listview_20200530/MainActivity.kt
@@ -2,11 +2,18 @@ package dasdsa.sdn.listview_20200530
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dasdsa.sdn.listview_20200530.adapters.StudentAdapter
 import dasdsa.sdn.listview_20200530.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val students = ArrayList<Student>()
+
+    // oncreate 실행된 이후에 내용을 채워야함.
+    // 변수는 멤버변수로 만들어두면 차후에 편리함.
+    // 만드는건 지금 만들지만, 채우는건 나중에 하겠다.
+    lateinit var studentAdapter:StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +28,8 @@ class MainActivity : AppCompatActivity() {
         students.add(Student("이승원", 1978, true))
         students.add(Student("이현호", 1981, true))
 
+        studentAdapter = StudentAdapter(this, R.layout.student_list_item, students)
+
+        studentListView.adapter = studentAdapter
     }
 }
