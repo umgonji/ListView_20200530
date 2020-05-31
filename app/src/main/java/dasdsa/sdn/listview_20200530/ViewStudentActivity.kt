62a6d14699147580922c7b281f3cf30c2c127e1c
umgonji <umgonji@naver.com>
2020-05-31 10:31:25 +0900

    받아낸 학생정보를 활용 해서 데이터 출력

--- app/src/main/java/dasdsa/sdn/listview_20200530/ViewStudentActivity.kt
+++ app/src/main/java/dasdsa/sdn/listview_20200530/ViewStudentActivity.kt
@@ -3,6 +3,7 @@ package dasdsa.sdn.listview_20200530
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dasdsa.sdn.listview_20200530.datas.Student
+import kotlinx.android.synthetic.main.activity_view_student.*
 
 class ViewStudentActivity : AppCompatActivity() {
 
@@ -11,6 +12,17 @@ class ViewStudentActivity : AppCompatActivity() {
         setContentView(R.layout.activity_view_student)
 
         val student = intent.getSerializableExtra("student") as Student
+
+        nameTxt.text = student.name
+        ageTxt.text = "${student.getKoreanAge(2020)}세(${student.brithYear}"
+
+        if(student.isMale) {
+            gendetTxt.text = "남성"
+        }
+        else {
+            gendetTxt.text = "여성"
+        }
+
 
     }
 }
